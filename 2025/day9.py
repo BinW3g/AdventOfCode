28675cff4df52d2a674c4ed11c05842a3ada22b4
Florian Angerer <florian.angerer.2001@gmail.com>
2025-12-15 14:07:09 +0100

    2025 day9 part 2 started

--- 2025/day9.py
+++ 2025/day9.py
@@ -9,8 +9,44 @@ def part1(tiles):
   return (max_tile[1] - min_tile[1] + 1) * (max_tile[0] - min_tile[0] + 1)
 
 
+class Area:
+  def __init__(self, a, b):
+    if math.sqrt(a[0]**2 + a[1]**2) < math.sqrt(b[0]**2 + b[1]**2):
+      self.a = a
+      self.b = b
+    else:
+      self.a = b
+      self.b = a
+
+    self.area = (abs(b[1] - a[1]) + 1) * (abs(b[0] - a[0]) + 1)
+
+  def isPointInArea(self, p):
+    if (min(self.a[0], self.b[0]) < p[0] < max(self.a[0], self.b[0])
+        and min(self.a[1], self.b[1]) < p[1] < max(self.a[1], self.b[1])):
+      return True
+    return False
+
+  def __str__(self):
+    return '(' + str(self.a) + ' ' + str(self.b) + ' ' + str(self.area) + ')'
+
+
 def part2(tiles):
-  pass
+  areas = list()
+  for i, tile in enumerate(tiles):
+    for tile2 in tiles[i+1:]:
+      areas.append(Area(tile, tile2))
+
+  areas.sort(key=lambda x: x.area, reverse=True)
+  for area in areas:
+    found = True
+    for tile in tiles:
+      if area.isPointInArea(tile):
+        found = False
+        break
+    if found:
+      return area.area
+
+  return -1
 
 if __name__ == '__main__':
   positions = list()
